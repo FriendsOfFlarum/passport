d85cf79dab85aad57b57e1280d6e385ca2ccbde1
Ian Morland <ian@morland.me>
2020-12-15 19:53:35 +0000

    Update admin ux, replace deprecated event - beta 15

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,42 +1,52 @@
 import app from 'flarum/app';
-import { settings } from '@fof-components';
-
-const {
-    SettingsModal,
-    items: { StringItem },
-} = settings;
 
 app.initializers.add('fof-passport', app => {
-    app.extensionSettings['fof-passport'] = () => app.modal.show(
-        SettingsModal, {
-            title: app.translator.trans('fof-passport.admin.popup.title'),
-            type: 'medium',
-            items: s => [
-                <StringItem setting={s} name='fof-passport.app_auth_url' placeholder='https://example.com/oauth/authorize'>
-                    {app.translator.trans('fof-passport.admin.popup.field.app-auth-url')}
-                </StringItem>,
-                <StringItem setting={s} name='fof-passport.app_token_url' placeholder='https://example.com/oauth/token'>
-                    {app.translator.trans('fof-passport.admin.popup.field.app-token-url')}
-                </StringItem>,
-                <StringItem setting={s} name='fof-passport.app_user_url' placeholder='https://example.com/api/user'>
-                    {app.translator.trans('fof-passport.admin.popup.field.app-user-url')}
-                </StringItem>,
-                <StringItem setting={s} name='fof-passport.app_id' placeholder='123'>
-                    {app.translator.trans('fof-passport.admin.popup.field.app-id')}
-                </StringItem>,
-                <StringItem setting={s} name='fof-passport.app_secret' placeholder='abcdefghijABCDEFGHIJabcdefghijABCDEFGHIJ'>
-                    {app.translator.trans('fof-passport.admin.popup.field.app-secret')}
-                </StringItem>,
-                <StringItem setting={s} name='fof-passport.app_oauth_scopes'>
-                    {app.translator.trans('fof-passport.admin.popup.field.app-scopes')}
-                </StringItem>,
-                <StringItem setting={s} name='fof-passport.button_title' placeholder={app.translator.trans('fof-passport.admin.popup.field.button-title-placeholder')}>
-                    {app.translator.trans('fof-passport.admin.popup.field.button-title')}
-                </StringItem>,
-                <StringItem setting={s} name='fof-passport.button_icon' placeholder='far fa-id-card'>
-                    {app.translator.trans('fof-passport.admin.popup.field.button-icon')}
-                </StringItem>
-            ]
-        }
-    );
+    app.extensionData.for('fof-passport')
+        .registerSetting({
+            label: app.translator.trans('fof-passport.admin.popup.field.app-auth-url'),
+            setting: 'fof-passport.app_auth_url',
+            type: 'text',
+            placeholder: 'https://example.com/oauth/authorize',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-passport.admin.popup.field.app-token-url'),
+            setting: 'fof-passport.app_token_url',
+            type: 'text',
+            placeholder: 'https://example.com/oauth/token',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-passport.admin.popup.field.app-user-url'),
+            setting: 'fof-passport.app_user_url',
+            type: 'text',
+            placeholder: 'https://example.com/api/user',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-passport.admin.popup.field.app-id'),
+            setting: 'fof-passport.app_id',
+            type: 'text',
+            placeholder: '123',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-passport.admin.popup.field.app-secret'),
+            setting: 'fof-passport.app_secret',
+            type: 'text',
+            placeholder: 'abcdefghijABCDEFGHIJabcdefghijABCDEFGHIJ',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-passport.admin.popup.field.app-scopes'),
+            setting: 'fof-passport.app_oauth_scopes',
+            type: 'text',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-passport.admin.popup.field.button-title'),
+            setting: 'fof-passport.button_title',
+            type: 'text',
+            placeholder: app.translator.trans('fof-passport.admin.popup.field.button-title-placeholder'),
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-passport.admin.popup.field.button-icon'),
+            setting: 'fof-passport.button_icon',
+            type: 'text',
+            placeholder: 'far fa-id-card',
+        });
 });
